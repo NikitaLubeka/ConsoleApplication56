755932b23ff29383e4961bc04fa1db601f50f05f
NikitaLubeka <133245296+NikitaLubeka@users.noreply.github.com>
2023-07-08 14:56:49 +0300

    Add files via upload

---
+++ Проект команы БЕТА.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+double convertLength(double value, int from, int to) {
+    double ratios[] = { 1.0, 0.01, 0.001, 1000.0, 1609.34 };
+    return value * ratios[from] / ratios[to];
+}
+
+double convertMass(double value, int from, int to) {
+    double ratios[] = { 1.0, 0.001, 0.000001, 1000.0 };
+    return value * ratios[from] / ratios[to];
+}
+
+double convertVolume(double value, int from, int to) {
+    double ratios[] = { 1.0, 0.001, 0.000001, 1000.0, 0.001, 1000000.0 };
+    return value * ratios[from] / ratios[to];
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int choice;
+
+    do {
+        cout << "Выберите вариант конвертации:\n"
+            << "1. Длина\n"
+            << "2. Масса\n"
+            << "3. Объем\n"
+            << "0. Выход\n"
+            << "Введите номер варианта: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            int from, to;
+            double value;
+            cout << "Введите значение: ";
+            cin >> value;
+            cout << "Выберите исходную единицу:\n"
+                << "1. Метры\n"
+                << "2. Сантиметры\n"
+                << "3. Миллиметры\n"
+                << "4. Километры\n"
+                << "5. Мили\n"
+                << "Введите номер единицы: ";
+            cin >> from;
+            cout << "Выберите целевую единицу:\n"
+                << "1. Метры\n"
+                << "2. Сантиметры\n"
+                << "3. Миллиметры\n"
+                << "4. Километры\n"
+                << "5. Мили\n"
+                << "Введите номер единицы: ";
+            cin >> to;
+
+            string units[] = { "м", "см", "мм", "км", "мили" };
+
+            double result = convertLength(value, from, to);
+            cout << value << " " << units[from - 1] << " = " << result << " " << units[to - 1] << "\n";
+        }
+        else if (choice == 2) {
+            int from, to;
+            double value;
+            cout << "Введите значение: ";
+            cin >> value;
+            cout << "Выберите исходную единицу:\n"
+                << "1. Килограммы\n"
+                << "2. Граммы\n"
+                << "3. Миллиграммы\n"
+                << "4. Тонны\n"
+                << "Введите номер единицы: ";
+            cin >> from;
+            cout << "Выберите целевую единицу:\n"
+                << "1. Килограммы\n"
+                << "2. Граммы\n"
+                << "3. Миллиграммы\n"
+                << "4. Тонны\n"
+                << "Введите номер единицы: ";
+            cin >> to;
+
+            string units[] = { "кг", "г", "мг", "т" };
+            double result = convertMass(value, from, to);
+            cout << value << " " << units[from - 1] << " = " << result << " " << units[to - 1] << "\n";
+        }
+        else if (choice == 3) {
+            int from, to;
+            double value;
+            cout << "Введите значение: ";
+            cin >> value;
+            cout << "Выберите исходную единицу:\n"
+                << "1. Литры\n"
+                << "2. Миллилитры\n"
+                << "3. Микролитры\n"
+                << "4. Кубические метры\n"
+                << "5. Кубические сантиметры\n"
+                << "6. Кубические миллиметры\n"
+                << "Введите номер единицы: ";
+            cin >> from;
+            cout << "Выберите целевую единицу:\n"
+                << "1. Литры\n"
+                << "2. Миллилитры\n"
+                << "3. Микролитры\n"
+                << "4. Кубические метры\n"
+                << "5. Кубические сантиметры\n"
+                << "6. Кубические миллиметры\n"
+                << "Введите номер единицы: ";
+            cin >> to;
+
+            string units[] = { "л", "мл", "мкл", "м³", "см³", "мм³" };
+            double result = convertVolume(value, from, to);
+            cout << value << " " << units[from - 1] << " = " << result << " " << units[to - 1] << "\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
