56f11178d9502bf45239b7b52cd8f1ff628948f3
NikitaLubeka <133245296+NikitaLubeka@users.noreply.github.com>
2024-01-28 11:22:22 +0200

    Add files via upload

---
+++ Дз3452.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть перше число:");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введiть відсоток:");
+        double percentage = Convert.ToDouble(Console.ReadLine());
+
+        double result = CalculatePercentage(number, percentage);
+
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static double CalculatePercentage(double number, double percentage)
+    {
+        return (number * percentage) / 100;
+    }
+}
+
+
